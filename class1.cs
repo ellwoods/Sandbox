bfa174deb8c1b165e2f0ea9d1dc179dce104ab35
S Ellwood <steve.ellwood@doncaster.gov.uk>
2022-08-05 10:00:24 +0100

    demo in dev

---
+++ class1.cs
@@ -0,0 +1,12 @@
+public class myclass {
+
+    public myclass()
+    {
+        // Work is completed. Now ready for test/release
+    }
+
+    public void method(){
+
+        //DIG-336	Make customer contact number mandatory within Bulky Collection processes
+    }
+}
